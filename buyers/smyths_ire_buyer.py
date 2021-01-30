7504f46798a2e58d296804f3e9a581175d2d9b32
Jack Cassidy <j.cassidy45@gmail.com>
2021-01-30 00:43:51 +0000

    add: retry logic to add to cart

--- buyers/smyths_ire_buyer.py
+++ buyers/smyths_ire_buyer.py
@@ -1,13 +1,15 @@
+import os
+
+from retrying import retry
 from selenium.common.exceptions import (
     NoSuchElementException,
     StaleElementReferenceException,
 )
 from selenium.webdriver.chrome.webdriver import WebDriver as ChromeDriver
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select, WebDriverWait
 
 from buyers.base_buyer import BaseBuyer
 from buyers.models.credit_card import CreditCard
@@ -17,11 +19,11 @@ class SmythsIreBuyer(BaseBuyer):
     def __init__(self, driver: ChromeDriver):
         super().__init__(
             driver,
-            "https://www.smythstoys.com/ie/en-ie/video-games-and-tablets/playstation-4/playstation-4-accessories/stealth-playstation-4-play-and-charge-dock/p/180333",
+            "https://www.smythstoys.com/ie/en-ie/video-games-and-tablets/playstation-5/playstation-5-consoles/playstation-5-console/p/191259",
         )
 
     def buy(self, credit_card: CreditCard):
-        self._driver.implicitly_wait(30)
+        self._driver.implicitly_wait(15)
 
         self._accept_cookies()
         self._login()
@@ -29,6 +31,7 @@ class SmythsIreBuyer(BaseBuyer):
         self._pay(credit_card)
 
     def _accept_cookies(self):
+        print("Accepting cookies")
         self._driver.get("https://www.smythstoys.com/ie/en-ie")
         self._driver.find_element_by_class_name("cookie-btn-yes").click()
         WebDriverWait(self._driver, 30).until(
@@ -37,6 +40,7 @@ class SmythsIreBuyer(BaseBuyer):
         print("Cookies accepted")
 
     def _login(self):
+        print("Logging in")
         self._driver.get("https://www.smythstoys.com/ie/en-ie/login")
         self._driver.find_element_by_id("j_username").send_keys(
             os.getenv("SMYTHS_USERNAME")
@@ -47,12 +51,16 @@ class SmythsIreBuyer(BaseBuyer):
         self._driver.find_element_by_id("loginSubmit").click()
         print("Logged in")
 
+    @retry(stop_max_delay=(1000 * 60 * 5), wait_fixed=(1000))
     def _add_to_cart(self):
+        print("Adding to cart")
         self._driver.get(self._ps5_page_url)
         self._driver.find_element_by_id("addToCartButton").click()
         print("Added to cart")
 
+    @retry(stop_max_delay=(1000 * 60 * 5), wait_fixed=(1000))
     def _pay(self, credit_card):
+        print("Paying")
         self._driver.get(
             "https://www.smythstoys.com/ie/en-ie/checkout/multi/delivery-address/add"
         )
